d2b95acad7caaf934eee8290cf9a68826f4275eb
Avi Reddy <222382855+AviReddy06@users.noreply.github.com>
2026-08-29 19:48:30 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (31.71%) - LeetSync

--- 2491-smallest-even-multiple/smallest-even-multiple.cpp
+++ 2491-smallest-even-multiple/smallest-even-multiple.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     int smallestEvenMultiple(int n) {
-        int res = 0;
         if(n%2==0){
             return n;
         }else{
-            res = n*2;
-            return res;
+            return n*2;
         }
     }
 };
