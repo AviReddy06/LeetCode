c66b13eeb5f9ed5be1fd31a965c0488d0b38679e
Avi Reddy <222382855+AviReddy06@users.noreply.github.com>
2026-09-03 11:33:58 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (37.04%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int l = -1;
+        // int l = -1;
         for(int i = 0;i<nums.size();i++){
             if(nums[i]==target){
-                l = i;
+                return i;
                 // count++;
                 break;
             }
         }
-        return l;
+        return -1;
         
     }
 };
