840959dca89ba1fb1d0e45f61530e57811dd3247
Avi Reddy <222382855+AviReddy06@users.noreply.github.com>
2026-09-15 12:16:25 +0530

    Time: 7 ms (51.52%) | Memory: 13.1 MB (89.63%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
-    bool isvowel(char c){
-        return (c=='a'||c=='e'||c=='i'||c=='o'||c=='u');
+    bool isvowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
     }
+
     int maxVowels(string s, int k) {
-        int count = 0,max_count = 0;
-        for(int i = 0;i<k;i++){
-            if(isvowel(s[i])==true){
-                count++;
+        int max_count{ 0 };
+        int current_count{ 0 };
+        for (int i{ 0 }; i <= k - 1; ++i) {
+            if (isvowel(s[i])) {
+                ++current_count;
             }
         }
-        max_count = count;
-        for(int i = k;i<s.size();i++){
-            if(isvowel(s[i])==true) count++;
-            if(isvowel(s[i-k])==true) count--;
-
-            max_count = std::max(max_count,count);
+        max_count = current_count;
+        for (int left{ 1 }; left <= s.length() - k; ++left) {
+            if (isvowel(s[left - 1])) {
+                --current_count;
+            }
+            if (isvowel(s[left + k - 1])) {
+                ++current_count;
+            }
+            if (current_count > max_count) {
+                max_count = current_count;
+            }
         }
-
         return max_count;
     }
 };
